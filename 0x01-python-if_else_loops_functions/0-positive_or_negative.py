31e76cd942389e56a0fe65ec6f8db2044f06c0b0
Eze Israel John <ezeisrael499@gmail.com>
2023-11-28 21:05:56 +0100

    Added a script that checks for positive or negative value

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+
+# Condition to check positive or negative value
+if number > 0:
+    print(f"{number} is positive")  # print this if number is positive
+elif number == 0:
+    print(f"{number} is zero")  # print this if number is zero
+else:
+    print(f"{number} is negative")  #print this if number is negative
