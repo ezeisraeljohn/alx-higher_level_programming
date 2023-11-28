885f8dc00a0f784e2506f1853ad446bbee3dbceb
Eze Israel John <ezeisrael499@gmail.com>
2023-11-28 23:09:43 +0100

    Added a script that prints numbers from 1 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 for i in range(0, 100):
     if i != 99:
         cus_string = "{}, ".format(i) if i > 9 else "{}, ".format("0" + str(i))
-        print(cus_string, end = "")
+        print(cus_string, end="")
     if i == 99:
         print(i)
