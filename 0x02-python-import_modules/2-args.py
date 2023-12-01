fce0dcf6af5b2a3452d8a640f5aee0f697b80dc0
Eze Israel John <ezeisrael499@gmail.com>
2023-12-01 02:02:38 +0100

    Added a python script that prints command line args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,32 +1,13 @@
 #!/usr/bin/python3
-from sys import argv
-
-
-def num_of_args():
-    """ function that list out all command line arguements
-
-    Return: Nothing
-
-    """
-    args_len = len(argv)
-
-    # if arguement other than filename, print the index followed the the name
-    if args_len == 2:
-        print("{} argument:".format(argv.index(argv[1])))
-        print("{}: {}".format(argv.index(argv[1]), argv[1]))
-
-    # if arguement is filename only, print index followed by the name
-    elif args_len == 1:
-        print("{} arguments.".format(argv.index(argv[0])))
-
-    # Do this if two conditions above are not met
+if __name__ == "__main__":
+    import sys
+    num = len(sys.argv)
+    if num == 1:
+        print("{} arguments.".format(num - 1))
+    elif num == 2:
+        print("{} argument:".format(num - 1))
     else:
-        print("{} arguments:".format(args_len))
-        for index in range(args_len):
-            print("{}: {}".format(int(argv.index(argv[index]) + 1),
-                                  argv[index]))
+        print("{} arguments:".format(num - 1))
 
-
-# Don't call the function if this module is imported
-if __name__ == '__main__':
-    num_of_args()
+    for i in range(1, num):
+        print("{}: {}".format(i, sys.argv[i]))
