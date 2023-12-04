6f0b5a873a286936ab078503d595294169be7c89
Eze Israel John <ezeisrael499@gmail.com>
2023-12-04 13:51:55 +0100

    Added a function that deletes an item in a list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    if my_list:
-
-        if idx < 0 or idx > len(my_list):
-            return my_list
+    if not my_list:
+        return my_list
+    if idx < 0 or idx >= len(my_list):
+        return my_list
 
     del my_list[idx]
     return my_list
