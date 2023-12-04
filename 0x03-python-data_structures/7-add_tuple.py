1afc23434dafda0fa625d66b2b0a1c5148552924
Eze Israel John <ezeisrael499@gmail.com>
2023-12-04 08:36:19 +0100

    Added a function that prints adds tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -18,16 +18,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
     a_first = int(tuple_a[0]) if len(tuple_a) > 0 else 0
     a_second = int(tuple_a[1]) if len(tuple_a) > 1 else 0
 
-    b_first = int(tuple_b[0]) if len(tuple_b) > 0 else 0
-    b_second = int(tuple_b[1]) if len(tuple_b) > 1 else 0
+    b_first = (tuple_b[0]) if len(tuple_b) > 0 else 0
+    b_second = (tuple_b[1]) if len(tuple_b) > 1 else 0
 
     el1 = a_first + b_first
     el2 = a_second + b_second
 
     return (el1, el2)
-
-
-tuple_a = (1,)
-tuple_b = ()
-
-print(add_tuple(tuple_a, tuple_b))
