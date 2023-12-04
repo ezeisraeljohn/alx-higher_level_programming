ab557587bdad76fe94ae48bc4b0c2a07559beef0
Eze Israel John <ezeisrael499@gmail.com>
2023-12-04 11:16:17 +0100

    Added a function that finds the max number of a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    """ Function that returns the maximum number in a list
+
+        args:
+        my_list
+
+        Return: The maximum number in a list
+
+    """
+    if not my_list:
+        return None
+
+    max = 0
+    for item in my_list:
+        if int(item) > max:  # assign item to max if it is greater than max
+            max = item
+
+    return max
