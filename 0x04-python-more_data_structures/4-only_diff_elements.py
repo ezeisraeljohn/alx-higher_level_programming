903064b2f28cfbd68cd844036e37f690ecfc14e5
Eze Israel John <ezeisrael499@gmail.com>
2023-12-06 17:11:41 +0100

    Added a function that returns the symmetric difference of sets

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    """ Function that returns the symmetric difference between two sets
+
+        args:
+        set_1 - The first set
+        set_2 - The second set
+
+        Return: The new set
+
+    """
+
+    return set_1 ^ set_2
