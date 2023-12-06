2686c1ccc98aa9fa0b419679031a1c019050787c
Eze Israel John <ezeisrael499@gmail.com>
2023-12-07 00:44:24 +0100

    Fixed some bugs

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    if not isinstance(roman_string, str):
-        return 0
-    total = 0
-    num = 0
-    digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    for r in reversed(roman_string):
-        num = digits[r]
-    total += num if total < num * 5 else -num
-    return total
+def square_matrix_simple(matrix=[]):
+    return list(map(lambda submat: list(map(lambda e: e**2, submat)), matrix))
