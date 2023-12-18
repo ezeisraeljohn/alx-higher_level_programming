7809152c2d21577a0e0af0aa988fd48cef1072aa
Eze Israel John <ezeisrael499@gmail.com>
2023-12-18 19:12:34 +0100

    Added some function that uses the try except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,7 +9,7 @@ def safe_print_list(my_list=[], x=0):
 
     try:
         for index in range(x):
-            print(f"{my_list[index]}", end="")
+            print("{}".format(my_list[index]), end="")
         print()
 
     except IndexError:
