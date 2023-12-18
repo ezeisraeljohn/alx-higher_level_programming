627fbff4f1e051d72fe70bf4fa8a4e2ef76eeb29
Eze Israel John <ezeisrael499@gmail.com>
2023-12-18 19:26:52 +0100

    Added the code that checks for indexError

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     count = 0
 
     for i in my_list:
-        count = count + 1  # Get the length of the string
+        count = count + 1  # Get the length of the list
 
     # run this block if no error (indexerror) occurs
     try:
