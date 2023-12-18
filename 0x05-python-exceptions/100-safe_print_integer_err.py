f886cdfe2d4f01cc4af7c421b08f6ac2d18bfcce
Eze Israel John <ezeisrael499@gmail.com>
2023-12-18 23:21:47 +0100

    Added a function that customizes exceptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,6 +7,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as error_message:
+    except (ValueError, TypeError) as error_message:
         sys.stderr.write("Exception: {}\n".format(error_message))
         return False
