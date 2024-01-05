a3ca30975a916b041eed215b0160ad51e7b31322
Eze Israel John <ezeisrael499@gmail.com>
2024-01-05 20:14:21 +0100

    Done

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    global count  = 0
-    return "BestSchool" if count "BestSchool ," * count
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
