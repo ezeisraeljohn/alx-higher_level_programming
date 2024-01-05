acf6ec602da546c76a41351613f51d765f01938f
Eze Israel John <ezeisrael499@gmail.com>
2024-01-05 11:52:50 +0100

    Few changes made

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -122,8 +122,9 @@ class Rectangle():
 
         if width < 0:
             raise ValueError("width must be >= 0")
-        if height < 0:
-            raise ValueError("height must be >= 0")
+
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
 
         new_instance = Rectangle(width, height)
         return new_instance
