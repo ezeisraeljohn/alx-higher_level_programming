eaef7d9b655a46a75cc86bcca9c254cea1fba8a7
Eze Israel John <ezeisrael499@gmail.com>
2024-01-05 12:18:34 +0100

    Added some functionality

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -88,7 +88,7 @@ class Rectangle():
 
     def __str__(self) -> str:
         """Function that makes use of the string method"""
-        if self.__width == 0 and self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return ""
         for i in range(self.__height):
             for j in range(self.__width):
