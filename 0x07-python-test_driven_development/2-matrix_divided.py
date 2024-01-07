612fa1c370b4827ce2da723215b38cd8042a9992
Eze Israel John <ezeisrael499@gmail.com>
2024-01-07 09:15:28 +0100

    Changes made

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
-""" This module contains function that multiplies element of a matrix"""
+""" This module contains function that multiplies element of a matrix
+    function(s):
+        matrix_divided: This function divides a matrix with a given number
+        passed as the second arguement of the function
+"""
 
 def matrix_divided(matrix, div):
     """Function That divides each element of a matrix by a number
@@ -8,7 +12,7 @@ def matrix_divided(matrix, div):
             matrix(list of list): This is the list of the matrix
             div(int): Number to divide elements of matrix with
         
-        Description: This function adds divides each element of the matrix,
+        Description: This function divides each element of the matrix,
                      and then the result will be in two decimal places
         
         Return: A new matrix containing the division
@@ -30,7 +34,7 @@ def matrix_divided(matrix, div):
         if len(matrix[i]) != length_matrix:
             raise TypeError("Each row of the matrix must have the same size")
 
-    # divide each integer of the array by 2 if all are of same type
+    # divide each integer of the matrix by div if all are of same type
     for row in range(len(new_matrix)):
         for column in range(length_matrix):
             if not isinstance(new_matrix[row][column], (int, float)):
