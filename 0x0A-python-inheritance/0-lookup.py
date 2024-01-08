63ea1a6faff7e0588290e2c59faa6d2c2562397a
Eze Israel John <ezeisrael499@gmail.com>
2024-01-08 17:07:03 +0100

    Added a function that prints the atr and meth of obj

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" This module contains function that returns the list of attr and methods
+    of a class
+"""
+def lookup(obj):
+    """ Function That returns the lists of atr and method of an object """
+    return dir(obj)
+
