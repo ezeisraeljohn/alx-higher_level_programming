ddb31b155efa22f120d9985d2a64012cff99e372
Eze Israel John <ezeisrael499@gmail.com>
2024-01-09 00:34:50 +0100

    Made some changes

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+""" Module that contains The BaseGeometry class """
+
+
+class BaseGeometry():
+    """ An empty class
+        methods:
+            area(int): This is not implemented
+            integer_validator(int): Function that validates integers
+    """
+
+    def area(self):
+        """ Unimplemented area """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Function that validates integer
+
+            args:
+                name(str): The string
+                value(int): The int
+        """
+        # if not isinstance(name, str):
+        #     raise TypeError("must be a string")
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """ The Rectangle class
+
+        attributes:
+            width(int): the width of the rectangle
+            height(int): The height of the rectangle
+    """
+    def __init__(self, width, height):
+        """ Initializer method"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """ Returns the area of the rectangle """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """ The string function """
+        return f"{[Rectangle]} {self.__width}/{self.__height}"
