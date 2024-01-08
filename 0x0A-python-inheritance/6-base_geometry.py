81661b5fee9432e0e5e2752f8e7879bdb18aa5b7
Eze Israel John <ezeisrael499@gmail.com>
2024-01-08 20:12:38 +0100

    Added an empty class and an unimplemented area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" Module that contains an empty class"""
+
+
+class BaseGeometry():
+    """ An empty class
+        methods:
+            area(int): This is not implemented
+    """
+
+    def area(self):
+        """ Unimplemented area """
+        raise Exception("area() is not implemented")
