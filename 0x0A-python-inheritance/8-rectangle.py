9253eaa10da17de1f1a30241f36ad0063930f74e
Eze Israel John <ezeisrael499@gmail.com>
2024-01-09 00:08:26 +0100

    Added some functionality

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+""" Module that contains an empty class"""
+
+
+class BaseGeometry():
+    """ An empty class
+        methods:
+            area(int): This is not implemented
+            integer_validator(int): Function that validates integers
+    """
+
+    def area(self):
+        """ Unimplemented area """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Function that validates integer
+
+            args:
+                name(str): The string
+                value(int): The int
+        """
+        # if not isinstance(name, str):
+        #     raise TypeError("must be a string")
+
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+
+        if value <= 0:
+            raise TypeError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """ The Rectangle class
+
+        attributes:
+            width(int): the width of the rectangle
+            height(int): The height of the rectangle
+    """
+    def __init__(self, width, height):
+        """ Initializer method"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
