41bcf9b090a846f2bdb510f79337feac40ae9a3e
Eze Israel John <ezeisrael499@gmail.com>
2024-01-08 10:17:02 +0100

    Added the function that prints a square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -3,8 +3,9 @@
 """This module contains the function that prints a square in # representation
 """
 
+
 def print_square(size):
-    """"Function that prints a square in '#' form 
+    """"Function that prints a square in '#' form
 
         args:
             size(int | float): The size of the square
@@ -14,7 +15,7 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    
+
     for i in range(int(size)):
         for _ in range(int(size)):
             print("#", end="")
