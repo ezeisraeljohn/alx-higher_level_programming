333d538a0c21161f66efbc268dd7aa1d423d63c4
Eze Israel John <ezeisrael499@gmail.com>
2024-01-08 10:24:40 +0100

    Added the function that multiplies two matrix

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,40 +1,46 @@
 #!/usr/bin/python3
+""" Module that houses function that multiplies two matrices"""
+
 
 def matrix_mul(m_a, m_b):
-    
+    """ The function that multiplies two matrices
+
+        args:
+            m_a([[]]): The first matrix
+            m_a([[]]): The second matrix
+    """
+
     if not isinstance(m_a, list):
         raise TypeError("m_a must be a list of lists")
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list of lists")
-    
+
     for row1 in m_a:
         if not isinstance(row1, list):
             raise TypeError("m_a must be a list of lists")
-        
+
     for row2 in m_b:
         if not isinstance(row2, list):
             raise TypeError("m_b must be a list of lists")
-    
+
     if not m_a:
         raise ValueError("m_a can't be empty")
-    
+
     if not m_b:
         raise ValueError("m_b can't be empty")
-    
+
     count = 0
     for i in range(len(m_a)):
         if len(m_a[i]) == 0:
             count += 1
-    
+
     if count == len(m_a):
-        raise ValueError("m_b can't be empty")
-    
-    count = 0
+        raise ValueError("m_a can't be empty")
+
+    count2 = 0
     for j in range(len(m_b)):
-        if len(m_a[j]) == 0:
-            count += 1
-    
-    if count == len(m_b):
+        if len(m_b[j]) == 0:
+            count2 += 1
+
+    if count2 == len(m_b):
         raise ValueError("m_b can't be empty")
-    
-    
