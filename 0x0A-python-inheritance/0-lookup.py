ab397a892222c66d5f20d87eeb4d4df9e91a8adf
Eze Israel John <ezeisrael499@gmail.com>
2024-01-08 17:08:34 +0100

    Pycodestyle checked

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,7 +2,8 @@
 """ This module contains function that returns the list of attr and methods
     of a class
 """
+
+
 def lookup(obj):
     """ Function That returns the lists of atr and method of an object """
     return dir(obj)
-
