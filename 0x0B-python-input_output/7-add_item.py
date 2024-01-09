222086898ad56cca7548a7eb20eca3c2611fd7c2
Eze Israel John <ezeisrael499@gmail.com>
2024-01-09 16:42:25 +0100

    Added some changes

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" The module that handle command line"""
+
+
+argv = __import__('sys').argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+json_list = [argv[i] for i in range(len(argv)) if i != 0]
+
+filename = 'add_item.json'
+
+save_to_json_file(json_list, filename)
+
+load_from_json_file(filename)
