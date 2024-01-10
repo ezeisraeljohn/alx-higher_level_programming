131ec2b601e2d8be0c720bff9d4446f708535947
Eze Israel John <ezeisrael499@gmail.com>
2024-01-10 17:35:04 +0100

    Changes made

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,22 +2,24 @@
 
 """This module contains function that adds two numbers"""
 
+
 def add_integer(a, b=98):
     """ Function that adds two numbers
 
         args:
             num1 (int): The first number to be added
             num2 (int): The second number to be added
-        
+
         Description:
             The function adds two numbers only if the two numbers passed into
-            it are float or int. if it is a float, it is first typcasted into an integer and then added.
+            it are float or int. if it is a float, it is first typcasted into
+            an integer and then added.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
+
     convert_to_int = (int(a), int(b))
 
     return convert_to_int[0] + convert_to_int[1]
