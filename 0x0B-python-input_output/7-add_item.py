100d7eb55cb54d8a25b4528ff50ecdc8bfebef50
Eze Israel John <ezeisrael499@gmail.com>
2024-01-10 15:45:03 +0100

    Fixed the bug

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,16 +2,20 @@
 
 """ The module that handle command line"""
 
-
+path = __import__('os').path
 argv = __import__('sys').argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
+filename = "add_item.json"
 json_list = [argv[i] for i in range(len(argv)) if i != 0]
 
-filename = 'add_item.json'
-
-save_to_json_file(json_list, filename)
-
-result = load_from_json_file(filename)
+if path.isfile(filename):
+    if path.getsize(filename) == 0:
+        save_to_json_file(json_list, filename)
+    else:
+        line = load_from_json_file(filename)
+        for item in json_list:
+            line.append(item)
+        save_to_json_file(line, filename)
