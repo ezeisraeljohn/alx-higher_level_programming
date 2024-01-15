904e91b17e326966468af8532a5239b230ea46b5
Eze Israel John <ezeisrael499@gmail.com>
2024-01-15 23:32:56 +0100

    Changes added

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,5 +9,39 @@ class Square(Rectangle):
     """ The Square Class"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
-        
-    
+
+    def __str__(self):
+        return (f"[Square] ({self.id}) {self.x}/{self.y} - "
+                f"{self.width}")
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        self.width = size
+
+    def update(self, *args, **kwargs):
+        """ Updating attributes methods
+
+        Args:
+            args(int): The integer arguements to update attribute with
+
+        """
+        if args:
+            attrs = ['id', 'size', 'x', 'y']
+            for attr, value in zip(attrs, args):
+                if hasattr(self, attr):
+                    setattr(self, attr, value)
+
+        else:
+            for attr, value in kwargs.items():
+                if hasattr(self, attr):
+                    setattr(self, attr, value)
+
+    def to_dictionary(self):
+        """ Return the dictionary of an instance"""
+        attrs = ['id', 'size', 'x', 'y']
+        return {attr: getattr(self, attr) for attr in attrs
+                if hasattr(self, attr)}
