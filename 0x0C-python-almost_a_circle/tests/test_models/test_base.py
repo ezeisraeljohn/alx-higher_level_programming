dc310fd0e9d2a5323c4564fa46c1794668b64f4e
Eze Israel John <ezeisrael499@gmail.com>
2024-01-15 01:59:20 +0100

    Changes made

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -6,16 +6,16 @@ from models.base import Base
 class TestBaseClass(unittest.TestCase):
 
     def setUp(self) -> None:
-      self.base1 = Base()
-      self.base2 = Base()
-      self.base3 = Base()  
-    
+        self.base1 = Base()
+        self.base2 = Base()
+        self.base3 = Base()
+
     def test_num_of_obj(self):
-       self.assertEqual(self.base3.id, self.base2.id + 1)
+        self.assertEqual(self.base3.id, self.base2.id + 1)
 
     def test_num_of_obj2(self):
-       self.assertEqual(self.base3.id, self.base1.id + 2)
+        self.assertEqual(self.base3.id, self.base1.id + 2)
 
     def test_accessing_nb_objects(self):
         with self.assertRaises(AttributeError):
-           Base.__nb_objects
+            Base.__nb_objects
