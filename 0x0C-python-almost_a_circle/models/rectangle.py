d0e03a812471832987a2f8f491e7ca603d3d387e
Eze Israel John <ezeisrael499@gmail.com>
2024-01-15 02:09:43 +0100

    Added some changes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,13 +10,13 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
-        if not isinstance(height,int):
+        if not isinstance(height, int):
             raise TypeError("height must be an integer")
         if not isinstance(x, int):
             raise TypeError("x must be an integer")
         if not isinstance(y,int):
             raise TypeError("y must be an integer")
-        
+
         if width <= 0:
             raise ValueError("width must be > 0")
         if height <= 0:
@@ -34,59 +34,59 @@ class Rectangle(Base):
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
-        
+
         self.__width = value
 
     @property
     def height(self):
         return self.__height
-    
+
     @width.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
-        
+
         self.__height = value
 
     @property
     def x(self):
         return self.__x
-    
+
     @x.setter
     def x(self, value):
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
-        
+
         self.__x = value
-    
+
     @property
     def y(self):
         return self.__y
-    
+
     @y.setter
     def y(self, value):
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
-        
+
         self.__y = value
-    
+
     def area(self):
         """ Area that calculates the area of Rectangle instance"""
         return self.__width * self.__height
-    
+
     def display(self):
         """Function that prints area of the rectangle with Squares"""
         if self.__width or self.__height == 0:
@@ -96,6 +96,7 @@ class Rectangle(Base):
             for j in range(self.__width):
                 print("#", end="")
             print()
-    
+
     def __str__(self):
-        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+        return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - "
+                f"{self.__width}/{self.__height}")
