f57dc25efe9267f8fc94d2f1a23485780aaad59c
Eze Israel John <ezeisrael499@gmail.com>
2024-01-16 00:02:25 +0100

    Some changes added

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,10 @@
 """ The Module Documentation """
 
 
+import json
+import os
+
+
 class Base():
     """ The base class of this project
 
@@ -22,3 +26,40 @@ class Base():
         else:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Converts list_dictionaries to json serializaion"""
+        if list_dictionaries is None or not list_dictionaries:
+            return []
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ Class that saves to a file
+            Args:
+                list_objs: The list of objects to be converted to dictionary
+        """
+        dic_list = [obj.to_dictionary() for obj in list_objs]
+        cls.to_json_string(dic_list)
+
+        with open(f'{cls.__name__}.json', 'w', encoding='utf-8') as file:
+            json.dump(dic_list, file)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ Deserializes a json string
+
+            Args:
+                json_string: The Json strings
+        """
+        if json_string is None or not json_string:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ Added the create method """
+        dummy = cls(1, 5)
+        dummy.update(**dictionary)
+        return dummy
