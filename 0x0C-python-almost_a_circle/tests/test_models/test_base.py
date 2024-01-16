fb232ab0ae5836fdbc40a4eff3dadf711a3c4b97
Eze Israel John <ezeisrael499@gmail.com>
2024-01-16 01:19:30 +0100

    Added some test for the base class

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -19,7 +19,23 @@ class TestBaseClass(unittest.TestCase):
     def test_accessing_nb_objects(self):
         with self.assertRaises(AttributeError):
             Base.__nb_objects
- 
+
     def test_passing_unrequired_arg(self):
         with self.assertRaises(TypeError):
             Base(3, 2)
+  
+    def test_from_json_string(self):
+        # Test case when the input is a valid JSON string
+        json_str = '[{"name": "John", "age": 25},{"name": "Alice", "age": 30}]'
+        result = Base.from_json_string(json_str)
+        expected_result = [{'name': 'John', 'age': 25},
+                           {'name': 'Alice', 'age': 30}]
+        self.assertEqual(result, expected_result)
+
+        # Test case when the input is an empty JSON string
+        result_empty = Base.from_json_string('')
+        self.assertEqual(result_empty, [])
+
+        # Test case when the input is None
+        result_none = Base.from_json_string(None)
+        self.assertEqual(result_none, [])
