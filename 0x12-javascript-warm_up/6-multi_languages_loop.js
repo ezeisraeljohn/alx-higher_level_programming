db0c65b7083dbe8235c9584ac1bbd9e08ce87c65
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-11 18:02:11 +0100

    Added some changes

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
 
 while (arr[i]) {
