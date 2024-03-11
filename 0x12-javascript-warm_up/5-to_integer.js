973069c1b7c60e301b9b4df8eeacb6fb2c5379d0
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-11 17:02:40 +0100

    Made minor changes

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 if (Number(process.argv[2])) {
-  console.log(`My number ${process.argv[2]}`);
+  console.log(`My number: ${process.argv[2]}`);
 } else {
-  console.log('Not A Number');
+  console.log('Not a number');
 }
