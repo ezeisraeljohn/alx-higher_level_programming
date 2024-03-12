f95a7d13297cd3b61032df8c6a55f0dc70ab77cd
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-12 23:38:18 +0100

    Added some changes

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    if (w === 0 || w < 0 || h === 0 || h < 0 || !Number(w) || !Number(h)) {
+      return;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log();
+    }
+  }
+
+  rotate() {
+    const swap = this.height;
+    this.height = this.width;
+    this.width = swap;
+  }
+
+  double() {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(size) {
+    super(size, size);
+  }
+}
+
+module.exports = Square;
