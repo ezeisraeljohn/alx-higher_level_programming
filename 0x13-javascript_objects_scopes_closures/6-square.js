715ae4f030f1a3cffce706ba5655e70e1aad0d58
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-12 23:57:54 +0100

    Added some changes

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w === 0 || w < 0 || h === 0 || h < 0 || !Number(w) || !Number(h)) {
       return;
     } else {
