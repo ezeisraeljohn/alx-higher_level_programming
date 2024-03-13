d2e5fcf56243570718b6e94ad1f1720a554487e7
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-13 12:06:07 +0100

    Changes made

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,41 +1,9 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if (w === 0 || w < 0 || h === 0 || h < 0 || !Number(w) || !Number(h)) {
-      return;
-    } else {
-      this.width = w;
-      this.height = h;
-    }
-  }
+const Square5 = require('./5-square');
 
-  print() {
-    for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
-        process.stdout.write('X');
-      }
-      console.log();
-    }
-  }
-
-  rotate() {
-    const swap = this.height;
-    this.height = this.width;
-    this.width = swap;
-  }
-
-  double() {
-    this.height = this.height * 2;
-    this.width = this.width * 2;
-  }
-}
-
-class Square extends Rectangle {
-  constructor(size) {
-    super(size, size);
-  }
-  charPrint(c) {
+class Square extends Square5 {
+  charPrint (c) {
     if (c === undefined) {
       super.print();
     } else {
@@ -48,5 +16,4 @@ class Square extends Rectangle {
     }
   }
 }
-
 module.exports = Square;
