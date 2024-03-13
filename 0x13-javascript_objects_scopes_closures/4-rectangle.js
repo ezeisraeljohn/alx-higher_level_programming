a166490af39df1c461d0c1b44dd6e4919bfe20dd
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-13 11:58:16 +0100

    Changes made

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -3,7 +3,7 @@
 class Rectangle {
   constructor (w, h) {
     if (w === 0 || w < 0 || h === 0 || h < 0 || !Number(w) || !Number(h)) {
-	return;
+        return;
     } else {
       this.width = w;
       this.height = h;
