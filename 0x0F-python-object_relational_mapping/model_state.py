1f92708c4102b58612c55c699d1022fec1e3e2fd
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-17 20:22:20 +0100

    Added the state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,10 +9,12 @@ from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     __tablename__ = 'states'
 
     id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
+
 
 engine = create_engine('mysql+mysqldb://localhost:3306', pool_pre_ping=True)
