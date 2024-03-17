c6c8fd6e28c78fb97fd54d904b6446115c341f2a
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-17 20:40:08 +0100

    Added the state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,35 +1,18 @@
 #!/usr/bin/python3
 
-""" The Documentation for this module """
-
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+""" Module that demonstrate the sqlalchemy """
 
 
-# Create the declarative base instance
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.orm import declarative_base
+
+
 Base = declarative_base()
 
 
 class State(Base):
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
 
-# Define the MySQL connection string
-MYSQL_USER = 'ezeisraeljohn'
-MYSQL_PASSWORD = '@israelEze12'
-MYSQL_HOST = 'localhost'
-MYSQL_PORT = '3306'
-MYSQL_DB = 'mydatabase'
-
-DB_URI = f'mysql+mysqldb://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}'
-
-# Create the engine
-engine = create_engine(DB_URI)
-
-# WARNING: Import all classes before calling Base.metadata.create_all(engine)
-
-if __name__ == "__main__":
-    # Create the tables in the database
-    Base.metadata.create_all(engine)
