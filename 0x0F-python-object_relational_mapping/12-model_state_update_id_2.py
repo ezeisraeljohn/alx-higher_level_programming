3ee2dfd9bb24e23e6408e9521da0fea8649b86b5
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-18 01:30:04 +0100

    Added a function that changes a state

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+""" The module that queries a database it is connected to using sqlalchemy"""
+
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sys import argv
+
+
+def change_state(username, password, database):
+    """ The function that queries the database and list all columns of
+        of State
+
+        Args:
+            username: The username of the user in a host
+            password: The passord of the user
+            database: The database to use
+    """
+    engine = create_engine(
+        f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}")
+
+    Session = sessionmaker(engine)
+
+    session = Session()
+
+    result = session.query(State).filter(State.id == 2).first()
+
+    result.name = "New Mexico"
+
+    session.commit()
+
+
+if __name__ == '__main__':
+    change_state(argv[1], argv[2], argv[3])
