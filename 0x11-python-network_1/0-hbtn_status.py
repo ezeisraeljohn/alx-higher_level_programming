8a160d5ceb3c4666ca67fcc69ab487083c8105ef
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-28 23:13:21 +0100

    Add script to retrieve and print the response from a URL

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import urllib.request
+
+with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
