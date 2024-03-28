c2eb2c3cc6c3dcaba3543b9f3ad699a4fec30a85
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-28 23:33:14 +0100

    Refactor code to include conditional check for main method

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+
+""" 1-hbtn_header.py """
+
+
 import sys
 import urllib.request
 
-with urllib.request.urlopen(f"{sys.argv[1]}") as response:
-    print(response.headers.get('X-Request-Id'))
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(f"{sys.argv[1]}") as response:
+        print(response.headers.get('X-Request-Id'))
