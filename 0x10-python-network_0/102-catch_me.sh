ed242f0f1bace4710b075d1e25d78f7bdaedc1ba
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-28 15:23:44 +0100

    Changes made

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a POST request to 0.0.0.0:5000/catch_me with the appropriate data
-curl -s -X PUT --data "user_id=98" 0.0.0.0:5000/catch_me
+curl -s -X PUT --data  0.0.0.0:5000/catch_me
