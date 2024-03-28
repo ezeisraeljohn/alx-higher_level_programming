a6450309e76c961c29e64f321186eaa7ddcf64e9
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-28 12:55:55 +0100

    Made some changes

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script will send a POST request to a URL with parameters
-curl -sIX POST "$1" -H "email: test@gmail.com" -H "subject: I will always be here for PLD"
+curl -sX POST "$1" -H "email: test@gmail.com" -H "subject: I will always be here for PLD"
