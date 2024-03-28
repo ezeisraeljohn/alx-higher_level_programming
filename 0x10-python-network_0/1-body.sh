31f2ee68cfe306a06c3825368d905c0b0222b4e0
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-28 10:55:30 +0100

    Made my files to be three lines

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash 
 # This script will display the body of the response
-curll=$(curl -sI "$1" | grep "HTTP/1.1 200 OK" |cut -d " " -f2)
-
-if [ "$curll" = "200" ]
-then
-       curl -s -X GET "$1"
-fi
+curl -sX GET "$1"
