83538a9a14bf5c0b454d964aa33912adede6f3ae
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-29 01:24:59 +0100

    Update content variable in print statement

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,4 +9,4 @@ if __name__ == '__main__':
     r = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r))
+    print("\t- content: {}".format(r.text))
