62da52f72e66dd397d3d737eb0c1bcb9decd41f2
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-29 15:47:12 +0100

    Fix JSON validation in search_user endpoint

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -13,10 +13,11 @@ if __name__ == "__main__":
         q = sys.argv[1]
     r = requests.post("http://0.0.0.0:5000/search_user", data={'q': q})
 
-    if not r.json():
-        print("Not a valid JSON")
     if type(r.json()) is dict:
         if r.json() == {}:
             print("No result")
         else:
             print("[{}] {}".format(r.json().get('id'), r.json().get('name')))
+
+    else:
+        print("Not a valid JSON")
