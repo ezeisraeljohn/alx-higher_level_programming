27e71a2f641085c9436dd2976516d1537cf2632d
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-29 10:46:35 +0100

    Add script to retrieve user ID from GitHub API

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" MY github API """
+
+import sys
+import requests
+
+if __name__ == "__main__":
+        r = requests.get('https://api.github.com/user', auth=(sys.argv[1], sys.argv[2]))
+        print(r.json().get('id'))
