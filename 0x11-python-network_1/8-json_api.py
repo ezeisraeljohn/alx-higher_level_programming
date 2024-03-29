08250162d2e39e23596d3350aee500ccd780555a
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-29 15:35:40 +0100

    Fix JSON validation and update output format

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -12,11 +12,11 @@ if __name__ == "__main__":
     else:
         q = sys.argv[1]
     r = requests.post("http://0.0.0.0:5000/search_user", data={'q': q})
+
+    if type(r.text) == str:
+        print("Not a valid JSON")
     if type(r.json()) is dict:
         if r.json() == {}:
             print("No result")
         else:
             print("[{}] {}".format(r.json().get('id'), r.json().get('name')))
-
-    else:
-        print("Not a valid JSON")
