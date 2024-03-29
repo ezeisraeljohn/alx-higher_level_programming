c10d0147d1c7f8b9fe0424ad598abd4a4065d06d
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-29 01:23:14 +0100

    Add request module and print response information

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+""" This uses the request module """
+
+import requests
+
+
+if __name__ == '__main__':
+    r = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r))
