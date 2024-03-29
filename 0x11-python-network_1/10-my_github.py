07f3a0ebbb6b43405d3d8ceefe5198621b8b3978
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-29 10:48:22 +0100

    Refactor API authentication in 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,5 +6,6 @@ import sys
 import requests
 
 if __name__ == "__main__":
-        r = requests.get('https://api.github.com/user', auth=(sys.argv[1], sys.argv[2]))
-        print(r.json().get('id'))
+    r = requests.get('https://api.github.com/user', auth=(sys.argv[1],
+                                                          sys.argv[2]))
+    print(r.json().get('id'))
