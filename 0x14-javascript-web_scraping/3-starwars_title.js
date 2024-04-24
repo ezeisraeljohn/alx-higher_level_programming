d3a13d3794e89306286cdaabf86553b050b37ef0
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-04-24 04:41:32 +0100

    Update requestMovieTitle function to include parameter description in 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,6 +4,11 @@ const request = require('request')
 const argvs = require('node:process').argv
 
 
+/**
+ * The requestMovieTitle function
+ * This function requests a movie title
+ * @param {number} id - The Id to be parsed
+ */
 function requestMovieTitle(id){
   request(`https://swapi-api.alx-tools.com/api/films/${id}`, (error, response, body)=>{
     if (error){
