94d3b9beda74d9e234cf7efd916094e4fea0823f
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-04-24 04:46:42 +0100

    Refactor requestMovieTitle function to use arrow function syntax in 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,22 +1,21 @@
 #!/usr/bin/node
 
-const request = require('request')
-const argvs = require('node:process').argv
-
+const request = require('request');
+const argvs = require('node:process').argv;
 
 /**
  * The requestMovieTitle function
  * This function requests a movie title
  * @param {number} id - The Id to be parsed
  */
-function requestMovieTitle(id){
-  request(`https://swapi-api.alx-tools.com/api/films/${id}`, (error, response, body)=>{
-    if (error){
-      console.error(error)
+function requestMovieTitle (id) {
+  request(`https://swapi-api.alx-tools.com/api/films/${id}`, (error, response, body) => {
+    if (error) {
+      console.error(error);
     }
-    const data = JSON.parse(body)
-    console.log(data.title)
-  } )
+    const data = JSON.parse(body);
+    console.log(data.title);
+  });
 }
 
-requestMovieTitle(argvs[2])
+requestMovieTitle(argvs[2]);
