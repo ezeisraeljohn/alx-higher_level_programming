a2dd54e2f01a09249b6d123f97329d5cdd988abb
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-04-24 05:35:59 +0100

    Add script to store web content from a URL in 5-request_store.js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+const argvs = require('node:process').argv;
+const fs = require('node:fs');
+
+/**
+ * storeWebContent function:
+ * This function stores the value of a web content into a file
+ * @param {Url} url 
+ * @param {file} filepath 
+ */
+function storeWebContent (url, filepath) {
+  request(url, (error, response, body) => {
+    if (error) {
+      console.error(error);
+    }
+    // Write the body content to the file
+    fs.writeFile(filepath, body, 'utf8', (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  });
+}
+storeWebContent(argvs[2], argvs[3]);
